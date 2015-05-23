bd85c5c301641f55d4db9e6ab64889dc8daa1cb8
Eric Stutzenberger <eric.stutzenberger@rigado.com>
2015-05-23 09:24:23 -0700

    update exceptions for nrf51822

--- src/exception.rs
+++ src/exception.rs
@@ -6,16 +6,8 @@ extern {
     pub fn nmi();
     /// All class of fault
     pub fn hard_fault();
-    /// Memory management
-    pub fn mem_manage();
-    /// Pre-fetch fault, memory access fault
-    pub fn bus_fault();
-    /// Undefined instruction or illegal state
-    pub fn usage_fault();
     /// System service call via SWI instruction
     pub fn svc();
-    /// Debug monitor
-    pub fn debug_mon();
     /// Pendable request for system service
     pub fn pend_sv();
     /// System tick timer
@@ -30,15 +22,15 @@ pub static VECTOR: [Option<unsafe extern fn()>; 16] = [
     /* 0x0004 */ Some(::reset),
     /* 0x0008 */ Some(nmi),
     /* 0x000C */ Some(hard_fault),
-    /* 0x0010 */ Some(mem_manage),
-    /* 0x0014 */ Some(bus_fault),
-    /* 0x0018 */ Some(usage_fault),
+    /* 0x0010 */ None,
+    /* 0x0014 */ None,
+    /* 0x0018 */ None,
     /* 0x001C */ None,
     /* 0x0020 */ None,
     /* 0x0024 */ None,
     /* 0x0028 */ None,
     /* 0x002C */ Some(svc),
-    /* 0x0030 */ Some(debug_mon),
+    /* 0x0030 */ None,
     /* 0x0034 */ None,
     /* 0x0038 */ Some(pend_sv),
     /* 0x003C */ Some(sys_tick),
