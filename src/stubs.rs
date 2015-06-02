8afe8ee3c296aa6bff7691b2b46696c3a4b8486d
Eric Stutzenberger <eric.stutzenberger@rigado.com>
2015-06-02 08:26:00 -0700

    add memset function from libc

--- src/stubs.rs
+++ src/stubs.rs
@@ -1,4 +1,8 @@
+
+extern {
+	pub fn memset(dest: *mut u32, value: u32, size: u32);
+}
 #[no_mangle]
 pub extern fn __aeabi_memset(_dest: *mut u32, _size: u32, _value: u32) {
-    ::abort()
+    memset(_dest, _value, _size);
 }
